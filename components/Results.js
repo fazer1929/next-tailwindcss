ba23cfd8b50c0587bcadf24461c879ce26b833c5
fazer1929 <abhishekagrawal8888@gmail.com>
2021-06-20 20:09:00 +0530

    dep ready

--- components/Results.js
+++ components/Results.js
@@ -1,12 +1,12 @@
 import Thubmnails from "./Thubmnails"
-
+import FlipMove from 'react-flip-move'
 function Results({results}) {
     return (
-        <div className="px-5 my-10 sm:grid md:grid-cols-2 xl:grid-cols-3 3xl:flex flex-wrap justify-center">
+        <FlipMove className="px-5 my-10 sm:grid md:grid-cols-2 xl:grid-cols-3 3xl:flex flex-wrap justify-center">
             {results.map(result=>(
                 <Thubmnails result={result} key={result.id}/>
             ))}
-        </div>
+        </FlipMove>
     )
 }
 
--- components/Thubmnails.js
+++ components/Thubmnails.js
@@ -1,10 +1,11 @@
 import Image from 'next/image'
 import logo from '../public/logo.png'
+import {forwardRef} from 'react'
 import {ThumbUpIcon} from '@heroicons/react/outline'
-function Thubmnails({result}) {
+const  Thubmnails = forwardRef(({result},ref)=> {
     const BASE_URL = "https://image.tmdb.org/t/p/original/"
     return (
-        <div className="group cursor-pointer p-2 transition duration-200 ease-in transform sm:hover:scale-105 hover:z-50">
+        <div ref={ref} className="group cursor-pointer p-2 transition duration-200 ease-in transform sm:hover:scale-105 hover:z-50">
             <Image
             layout='responsive'
             height={1080}
@@ -25,6 +26,6 @@ function Thubmnails({result}) {
             </div>
         </div>
     )
-}
+});
 
 export default Thubmnails
